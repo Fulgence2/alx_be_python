3210796e5298dd64bd9a7ad95acdc8628278c537
Fulgence <fulgenceondoro@gmail.com>
2025-05-23 23:12:49 +0300

    bug fixes

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,8 @@
-input = int(input("How old are you?:"))
+try:
+	input = int(input("How old are you?"))
+except ValueError:
+    print("Please enter a valid number.")
+
 age = input + 27
 
 print("In 2050, you will be ",age, "years old.")
