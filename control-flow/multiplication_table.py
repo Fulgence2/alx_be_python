a972057002e17a1f5ab306d101107aa4acf34362
Fulgence <fulgenceondoro@gmail.com>
2025-06-06 14:13:04 +0300

    Drawing Patterns with Nested Loops

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
-number = float(input("Enter a number to see its multiplication table: "))
-for num in range(1,11):
+number = int(input("Enter a number to see its multiplication table: "))
+for num in range(11):
     result = number * num
     print(f"{number} * {num} = {result}")
