e95264e0bd318053b75cae69d97851562368ef37
Fulgence <fulgenceondoro@gmail.com>
2025-06-06 18:59:23 +0300

    Update of the response view to json for readability

--- functions/weather.py
+++ functions/weather.py
@@ -1,11 +1,11 @@
 import requests
+import json
 
 key = "fee480808cc4414198c154102250606"
-city = "Nairobi"
+city = input("Enter your current city: ")
 
 weather_response = requests.get(url=f"https://api.weatherapi.com/v1/current.json?key={key}&q={city}&aqi=no")
 
-print(weather_response)
-print(weather_response.status_code)       # 200 (OK)
-print(weather_response.text)              # Raw response body
-print(weather_response.json())
+#Json response body
+data = weather_response.json()
+print(json.dumps(data, indent=4))
